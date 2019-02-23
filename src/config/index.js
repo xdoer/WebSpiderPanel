1823036541808d58f486a2d22c76974ca87b8896
LuckyHH <gotoanything@foxmail.com>
2019-02-23 15:22:03 +0800

    默认后端接口地址

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,7 @@
 export default {
   // 后端服务器地址
-  baseUrl: 'http://localhost:4000'
+  // Nginx 分发前端代码的话，请将null改为后端接口地址
+  baseUrl: null || (() => 
+    window.location.protocol + window.location.host
+  )()
 }
